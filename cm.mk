708af823f22acfc46c1adb6aa0b9330fd63ab126
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-03-30 19:31:53 -0400

    let's build Beanstalk
    Change-Id: I543f28b45a309244e48c50243ae12de89c7d6d88

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common beanstalk stuff.
+$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUDNJ2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUDNJ2 release-keys"
 
-PRODUCT_NAME := cm_d2spr
+PRODUCT_NAME := bs_d2spr
 PRODUCT_DEVICE := d2spr
 
